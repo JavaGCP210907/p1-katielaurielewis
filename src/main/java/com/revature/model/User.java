d24316f50fd05be35c842e569fe7ea82fab6269c
Katie Lewis <katielaurielewis@gmail.com>
2021-10-04 20:05:47 -0400

    some back end nonsense

--- src/main/java/com/revature/model/User.java
+++ src/main/java/com/revature/model/User.java
@@ -1,5 +1,88 @@
 package com.revature.model;
 
 public class User {
+	
+	private int id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private UserRole role;
+	
+	public User(int id, String username, String password, String firstName, String lastName, String email,
+			UserRole role) {
+		super();
+		this.setId(id);
+		this.setUsername(username);
+		this.setPassword(password);
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setEmail(email);
+		this.setRole(role);
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public UserRole getRole() {
+		return role;
+	}
+
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+	
+	
 
 }
